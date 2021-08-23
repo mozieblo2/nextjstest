8d8401114beeb50cff0386225fd4d6bd38a47270
Magdalena Oziębło <magdalena.ozieblo@itechart-group.com>
2021-08-23 14:15:05 +0200

    _app add code for all pages + getStaticProps added for read file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+     <div>
+       <p>Some text</p>
+       <Component {...pageProps} />
+     </div>
+  );
 }
 
 export default MyApp
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,29 @@
-const AllEventsPage = () => {
+import fs from 'fs/promises';
+import path from 'path';
+
+const AllEventsPage = (props) => {
+    const { products } = props
     return (
         <div>
             <h1>All events Page</h1>
+            <ul>
+                {products.map(product => <li key={product.id}>{product.product}</li>)}
+            </ul>
         </div>
     )
+}
+
+// STATIC SITE GENERATOR: executes before build and render component!
+export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-data.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            products: data.products
+        }
+    }
 }
 
 export default AllEventsPage;
